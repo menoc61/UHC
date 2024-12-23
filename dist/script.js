0171c618e41431c7c15e5066a5141dc2d88294b1
menoc61 <gillemomeni@gmail.com>
2024-12-24 02:52:24 +0300

    [Add] menu button animation

--- dist/script.js
+++ dist/script.js
@@ -14,15 +14,10 @@ function _defineProperty(obj, key, value) {
 
 console.clear();
 
-// there are 3 parts to this
-//
 // Scene:           Setups and manages threejs rendering
 // loadModel:       Loads the 3d obj file
 // setupAnimation:  Creates all the GSAP
 //                  animtions and scroll triggers
-//
-// first we call loadModel, once complete we call
-// setupAnimation which creates a new Scene
 
 class Scene {
   constructor(model) {
